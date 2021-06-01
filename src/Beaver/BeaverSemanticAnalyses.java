61e79b86ae07b6e393a97c13e6ee5df24afe6155
Diogo <diogo.correia99@ua.pt>
2021-06-01 01:58:24 +0100

    updated semantic analyses to boolean

--- src/Beaver/BeaverSemanticAnalyses.java
+++ src/Beaver/BeaverSemanticAnalyses.java
@@ -1,96 +1,96 @@
-public class BeaverInterpreter extends BeaverBaseVisitor<String> {
+public class BeaverInterpreter extends BeaverBaseVisitor<boolean> {
 
-   @Override public String visitProgram(BeaverParser.ProgramContext ctx) {
+   @Override public boolean visitProgram(BeaverParser.ProgramContext ctx) {
       return visitChildren(ctx);
    }
 
-   @Override public String visitContainers(BeaverParser.ContainersContext ctx) {
+   @Override public boolean visitContainers(BeaverParser.ContainersContext ctx) {
       return visitChildren(ctx);
    }
 
-   @Override public String visitStatsPallete(BeaverParser.StatsPalleteContext ctx) {
+   @Override public boolean visitStatsPallete(BeaverParser.StatsPalleteContext ctx) {
       return visitChildren(ctx);
    }
 
-   @Override public String visitStatsColor(BeaverParser.StatsColorContext ctx) {
+   @Override public boolean visitStatsColor(BeaverParser.StatsColorContext ctx) {
       return visitChildren(ctx);
    }
 
-   @Override public String visitStatsSet(BeaverParser.StatsSetContext ctx) {
+   @Override public boolean visitStatsSet(BeaverParser.StatsSetContext ctx) {
       return visitChildren(ctx);
    }
 
-   @Override public String visitStatsContains(BeaverParser.StatsContainsContext ctx) {
+   @Override public boolean visitStatsContains(BeaverParser.StatsContainsContext ctx) {
       return visitChildren(ctx);
    }
 
-   @Override public String visitIdsList(BeaverParser.IdsListContext ctx) {
+   @Override public boolean visitIdsList(BeaverParser.IdsListContext ctx) {
       return visitChildren(ctx);
    }
 
-   @Override public String visitInlineSet(BeaverParser.InlineSetContext ctx) {
+   @Override public boolean visitInlineSet(BeaverParser.InlineSetContext ctx) {
       return visitChildren(ctx);
    }
 
-   @Override public String visitExprMultDiv(BeaverParser.ExprMultDivContext ctx) {
+   @Override public boolean visitExprMultDiv(BeaverParser.ExprMultDivContext ctx) {
       return visitChildren(ctx);
    }
 
-   @Override public String visitExprAddSub(BeaverParser.ExprAddSubContext ctx) {
+   @Override public boolean visitExprAddSub(BeaverParser.ExprAddSubContext ctx) {
       return visitChildren(ctx);
    }
 
-   @Override public String visitExprTypeProperty(BeaverParser.ExprTypePropertyContext ctx) {
+   @Override public boolean visitExprTypeProperty(BeaverParser.ExprTypePropertyContext ctx) {
       return visitChildren(ctx);
    }
 
-   @Override public String visitExprParentesis(BeaverParser.ExprParentesisContext ctx) {
+   @Override public boolean visitExprParentesis(BeaverParser.ExprParentesisContext ctx) {
       return visitChildren(ctx);
    }
 
    // SEPARATION
    
-   @Override public String visitExprID(BeaverParser.ExprIDContext ctx) {
+   @Override public boolean visitExprID(BeaverParser.ExprIDContext ctx) {
       return visitChildren(ctx);
    }
 
-   @Override public String visitExprUnary(BeaverParser.ExprUnaryContext ctx) {
+   @Override public boolean visitExprUnary(BeaverParser.ExprUnaryContext ctx) {
       return visitChildren(ctx);
    }
 
-   @Override public String visitExprPower(BeaverParser.ExprPowerContext ctx) {
+   @Override public boolean visitExprPower(BeaverParser.ExprPowerContext ctx) {
       return visitChildren(ctx);
    }
 
-   @Override public String visitExprNumber(BeaverParser.ExprNumberContext ctx) {
+   @Override public boolean visitExprNumber(BeaverParser.ExprNumberContext ctx) {
       return visitChildren(ctx);
    }
 
-   @Override public String visitPointsCenter(BeaverParser.PointsCenterContext ctx) {
+   @Override public boolean visitPointsCenter(BeaverParser.PointsCenterContext ctx) {
       return visitChildren(ctx);
    }
 
-   @Override public String visitPointsExprCalc(BeaverParser.PointsExprCalcContext ctx) {
+   @Override public boolean visitPointsExprCalc(BeaverParser.PointsExprCalcContext ctx) {
       return visitChildren(ctx);
    }
 
-   @Override public String visitPointsExprPoint(BeaverParser.PointsExprPointContext ctx) {
+   @Override public boolean visitPointsExprPoint(BeaverParser.PointsExprPointContext ctx) {
       return visitChildren(ctx);
    }
 
-   @Override public String visitPointsExprExpr(BeaverParser.PointsExprExprContext ctx) {
+   @Override public boolean visitPointsExprExpr(BeaverParser.PointsExprExprContext ctx) {
       return visitChildren(ctx);
    }
 
-   @Override public String visitColor(BeaverParser.ColorContext ctx) {
+   @Override public boolean visitColor(BeaverParser.ColorContext ctx) {
       return visitChildren(ctx);
    }
 
-   @Override public String visitPoint(BeaverParser.PointContext ctx) {
+   @Override public boolean visitPoint(BeaverParser.PointContext ctx) {
       return visitChildren(ctx);
    }
 
-   @Override public String visitAngle(BeaverParser.AngleContext ctx) {
+   @Override public boolean visitAngle(BeaverParser.AngleContext ctx) {
       return visitChildren(ctx);
    }
 }
