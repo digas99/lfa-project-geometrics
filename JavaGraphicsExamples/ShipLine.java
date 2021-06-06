ff5ed188344672994aa99f8a809fd391eafe2010
Gonçalo Maranhão <goncalo.rodrigues@ua.pt>
2021-06-06 03:49:27 +0100

    Done. The logic could be better.

--- JavaGraphicsExamples/ShipLine.java
+++ JavaGraphicsExamples/ShipLine.java
@@ -1,42 +1,33 @@
-//////////////////////////---THIS IS NOT FINISHED--------------------------------
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.*;
 import java.awt.geom.Point2D;
-import java.awt.geom.Path2D;
-import java.util.ArrayList;
+import java.lang.Math;
 
 public class ShipLine extends JPanel implements ActionListener {
 
 
     final int Width_Board = 1000;
     final int Length_Board = 1000;  
-    Timer timer = new Timer(30,this);
+    Timer timer = new Timer(5,this);
     int velShipBody = 1;
-    int velTriangle = 1;
     double angle = 0.0;
-    // Point pointTopLeftRect = new Point(100,500);
-    // Point pointTopRightRect = new Point(pointTopLeftRect.getX()+ShipBody.getWidth(),pointTopLeftRect.getY()+ShipBody.getHeight());
     double xShipBody = 500;
     double HeightTriangle = 50;
-    double ySumRect;
-    double xSumRect;
-    double maxHeightFigure;
-    double maxWidthFIgure;
-    double varMinX = Width_Board;
-    double varMinY = Length_Board;
-    double varMaxX = Width_Board;
-    double varMaxY = Length_Board;
-    ArrayList<Double> minX = new ArrayList<Double>();
-    ArrayList<Double> minY = new ArrayList<Double>();
-    ArrayList<Double> maxWidth = new ArrayList<Double>();
-    ArrayList<Double> maxY = new ArrayList<Double>();
+    double maxRandAngle = 360;
+    double maxRandColor = 255;
+    int r=0;
+    int gr=0;
+    int b=0;
 
     public void paint(Graphics g){
 
+        super.paintComponent(g);        
 
-        super.paintComponent(g);
+        r=(int)(Math.random() * maxRandColor);
+        gr=(int)(Math.random() * maxRandColor);
+        b=(int)(Math.random() * maxRandColor);
 
         Graphics2D containerShip = (Graphics2D)g.create(); 
         
@@ -48,23 +39,20 @@ public class ShipLine extends JPanel implements ActionListener {
         Polygon triangle =  new Polygon();
         // This logic puts the triangle on top of rect(or any other figure)
         // it's made by me, so I'm not sure if it works for everything. 
-        // Bottom left point of triangle
+        // Bottom left(in this case) point of triangle
         triangle.addPoint((int)pointTopLeftRect.getX(),(int)pointTopLeftRect.getY());
-        // Bottom right point of triangle
+        // Bottom right(in this case) point of triangle
         triangle.addPoint((int)pointTopRightRect.getX(),(int)pointTopRightRect.getY());
 
         triangle.addPoint(((int)pointTopLeftRect.getX()+(int)pointTopRightRect.getX())/2, 
                          (int)pointTopLeftRect.getY()-(int)HeightTriangle);
 
-        containerShip.setColor(Color.BLACK);
-        containerShip.draw(ShipBody);
-        containerShip.setColor(Color.MAGENTA);
+        //containerShip.setColor(new Color(r,gr,b));
+        containerShip.setColor(new Color(r,gr,b));
         //This is thickness
         containerShip.setStroke(new BasicStroke(10));
-        containerShip.draw(triangle);
-
+   
         Rectangle RectTriangle = new Rectangle(triangle.getBounds());
-        Shape hiddenRect = RectTriangle;
 
         Rectangle2D sumRect = new Rectangle2D.Double(RectTriangle.getX(),RectTriangle.getY(),
                                                      ShipBody.getWidth(),ShipBody.getHeight()+RectTriangle.getHeight());
@@ -73,120 +61,57 @@ public class ShipLine extends JPanel implements ActionListener {
         containerShip.translate((int)sumRect.getCenterX(),(int)sumRect.getCenterY());
         containerShip.rotate(angle);
         containerShip.translate((int)-sumRect.getCenterX(),(int)-sumRect.getCenterY());
+
+        containerShip.fill(ShipBody);
+        containerShip.draw(triangle);
+    
+
         
-//////////////////--------------------------__Line Container -----------------------------/////////////////
+//////////////////--------------------------Line Container -----------------------------/////////////////
         
         Graphics2D containerLine = (Graphics2D)g.create();
         
-        containerLine.setColor(Color.RED);
+        containerLine.setColor(new Color(r,gr,b));
         containerLine.setStroke(new BasicStroke(20));
-        
-        // Could use points, as above
         Shape line = new Line2D.Double(0,50,1000,50);
         containerLine.draw(line);
 
-        /*
-            minX.add(ShipBody.getX());
-            minX.add(RectTriangle.getX());
-
-
-        for(double num : minX){
-            if(varMinX >= num) 
-            varMinX = num;
-        }
-            varMinY.add(ShipBody.getY());
-            varMinY.add(RectTriangle.getY());
-        
-        for(double num : minY){
-                if(varMinY >= num) 
-                varMinY = num;
-        }
-
-        maxWidth.add(ShipBody.getWidth());
-        maxWidth.add(RectTriangle.getWidth());
-
-        for(double num : maxWidth){
-            if(varMaxX >= num) 
-            varMaxX = num;
-        }
-
-        varMaxX = varMaxX + varMinX;
-
-        maxY.add(ShipBody.getHeight());
-        maxY.add(RectTriangle.getHeight());
-
-       /* for(double num : maxY){
-            if(varMaxY >= num) 
-            varMaxY = num;
-        }
-
-        varMaxY = varMaxY + varMinY;*/ 
-
-        
-
-        
-        //Eu quero o menor Y e o menor X da figura toda
-        /*if(ShipBody.getY() >= RectTriangle.getY()){
-            ySumRect = RectTriangle.getY();
-        }else{
-            ySumRect = ShipBody.getY();
-        }
-        if(ShipBody.getX() >= RectTriangle.getX()){
-            xSumRect = RectTriangle.getX();
-        }else{
-            xSumRect = ShipBody.getX();
-        }*/
-        
-
-       /* maxHeightFigure = RectTriangle.getHeight()+ ShipBody.getHeight()+ varMinY  ;
-        maxWidthFigure = varMaxX + varMinX; */
-
-        
-
         if(line.intersects(sumRect)){
-        containerLine.setColor(Color.BLUE);
-        velShipBody = 0;
-        containerLine.draw(line);
-        angle = 3;
-        containerShip.setColor(Color.BLACK);
-        containerShip.draw(ShipBody);
-        containerShip.draw(triangle);
-        }
-         
-        
+            velShipBody -= 1;
+            angle = Math.toRadians((Math.random() * maxRandAngle)); 
+            }
+
         containerShip.dispose();
         containerLine.dispose();
-        
-
 
         timer.start();
-    } 
-
-    
+        
+    }
+        
     @Override
     public void actionPerformed(ActionEvent e) {
-
+ 
         // This moves Rect and triangle at the same time because they share xShipBody
-        xShipBody -= velShipBody ;
-        
+        xShipBody -= velShipBody;
 
+        if( xShipBody == Width_Board-200){ 
+            velShipBody = velShipBody * -1;
+            angle = Math.toRadians((Math.random() * maxRandAngle));
+        }
 
         repaint();
-        
     }
 
     public static void main(String[] args) {
         ShipLine n = new ShipLine();
         JFrame frame1 = new JFrame();
         frame1.setTitle("Geometrics");
-        //frame1.setBackground(Color.MAGENTA);
-        frame1.setSize(1000,1000);
-        frame1.setVisible(true);
-        frame1.setLocationRelativeTo(null);
+        frame1.setSize(new Dimension(1000,1000));
         frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame1.add(n);
-    }
-    
+        frame1.setLocationRelativeTo(null);
+        frame1.setVisible(true);   
+    }    
 } 
 
 
